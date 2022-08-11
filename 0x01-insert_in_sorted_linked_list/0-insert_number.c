df2e3e1c72cd5eb38a5a9693c6746b4dac735178
maycolroa <lehallo@hotmail.com>
2022-08-11 15:19:56 -0500

    0x01. Insert in sorted linked list 11/08/22 0.00

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * insert_node - Function
+ * Return: new node, or NULL
+ * @head: Pointer to the pointer
+ * @number: Data
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+    
+}
