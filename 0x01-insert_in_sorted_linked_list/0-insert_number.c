6d010830aba3386d403d332391897e921d1a6e5c
maycol david roa triviño <85509333+maycolroa@users.noreply.github.com>
2022-08-11 22:24:14 -0500

    0x01. Insert in sorted linked list 0.05

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -11,5 +11,44 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
-    
+    listint_t *newnode;
+    listint_t *tmp = *head;
+
+    newnode = malloc(sizeof(listint_t));
+    if (newnode == NULL)
+    {
+        return (NULL);
+    }
+    newnode->n = number;
+    if (*head == NULL)
+    {
+        *head = newnode;
+        newnode->next = NULL;
+        return (newnode);
+    }
+    if (tmp->next == NULL)
+    {
+        newnode->next = NULL;
+        tmp->next = newnode;
+        return (newnode);
+    }
+    if (number <= tmp->n)
+    {
+        newnode->next = tmp;
+        *head = newnode;
+        return (newnode);
+    }
+    while (tmp->next)
+    {
+        if (number <= (tmp->next->n))
+        {
+            newnode->next = tmp->next;
+            tmp->next = newnode;
+            return (newnode);
+        }
+        tmp = tmp->next;
+    }
+    tmp->next = newnode;
+    newnode->next = NULL;
+    return (newnode);
 }
