a69bb76620cf4bd18357c84b3d511afa9745c30a
maycolroa <lehallo@hotmail.com>
2022-11-06 17:40:29 -0500

    0x0C. N Queens 02/11/22 0.02

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,44 +1,75 @@
 #!/usr/bin/python3
-"""
-import modules
-"""
+"""import modules"""
 import sys
 
 
-from sys import argv, exit
+global N
 
-if __name__ == "__main__":
-    if len(argv) != 2:
-        print("Usage: nqueens N")
-        exit(1)
-    try:
-        n = int(argv[1])
-    except BaseException:
-        print("N must be a number")
-        exit(1)
-    if n < 4:
-        print("N must be at least 4")
-        exit(1)
 
-    solution = []
+def printSolution(board):
+    """printsolution"""
+    q_pos = []
+    q_list = []
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] == 1:
+                q_pos.append(i)
+                q_pos.append(j)
+                q_list.append(list(q_pos))
+                q_pos.clear()
+    print(q_list)
 
-    def solve_queens(row, n, solution):
-        if (row == n):
-            print(solution)
-        else:
-            for col in range(n):
-                placement = [row, col]
-                if valid_placement(solution, placement):
-                    solution.append(placement)
-                    solve_queens(row + 1, n, solution)
-                    solution.remove(placement)
 
-    def valid_placement(solution, placement):
-        for queen in solution:
-            if queen[1] == placement[1]:
-                return False
-            if (queen[0] + queen[1]) == (placement[0] + placement[1]):
-                return False
-            if (queen[0] - queen[1]) == (placement[0] - placement[1]):
-                return False
+def isSafe(board, row, col):
+    """isSafe"""
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
+
+
+def solveNQUtil(board, col):
+    """solveNQUtl"""
+    if col >= N:
         return True
+    for i in range(N):
+        if isSafe(board, i, col):
+            board[i][col] = 1
+            if solveNQUtil(board, col + 1) is True:
+                printSolution(board)
+            board[i][col] = 0
+    return False
+
+
+def create_board(n):
+    """create_board"""
+    b_list = []
+    b_pos = []
+    for _ in range(n):
+        for _ in range(n):
+            b_pos.append(0)
+        b_list.append(list(b_pos))
+        b_pos.clear()
+    return b_list
+
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+
+try:
+    N = int(sys.argv[1])
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
+
+if N < 4:
+    print("N must be at least 4")
+    sys.exit(1)
+
+solveNQUtil(create_board(N), 0)
