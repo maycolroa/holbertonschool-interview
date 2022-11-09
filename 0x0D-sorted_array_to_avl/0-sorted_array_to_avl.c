e829718f6f37a9ebe0438d68240f5fb8a3ddae2a
maycolroa <lehallo@hotmail.com>
2022-11-09 16:59:17 -0500

    0x0D. Array to AVL 09/11/22 0.00

---
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+/**
+ * binary_search - recursive function
+ * @array: pointer to the first element
+ * @start: first element
+ * @end: last element
+ * @parent: node
+ * Return: a pointer
+ **/
+avl_t *binary_search(int *array, int start, int end, avl_t *parent)
