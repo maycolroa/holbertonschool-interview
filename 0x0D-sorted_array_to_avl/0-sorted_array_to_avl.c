4e3833de6c801fdb0b63015e767607b12e10f8d0
maycolroa <lehallo@hotmail.com>
2022-11-09 22:43:43 -0500

    0x0D. Array to AVL 09/11/22 0.01

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -9,3 +9,37 @@
  * Return: a pointer
  **/
 avl_t *binary_search(int *array, int start, int end, avl_t *parent)
+{
+	int middle_element;
+	avl_t *node;
+
+	if (start > end)
+	{
+		return (NULL);
+	}
+	middle_element = (start + end) / 2;
+	node = malloc(sizeof(avl_t));
+	if (!node)
+	{
+		return (NULL);
+	}
+	node->n = array[middle_element];
+	node->parent = parent;
+	node->left = binary_search(array, start, middle_element - 1, node);
+	node->right = binary_search(array, middle_element + 1, end, node);
+	return (node);
+}
+/**
+ * sorted_array_to_avl - Function array
+ * @array: pointer
+ * @size: array
+ * Return: Pointer
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	if (!array)
+	{
+		return (NULL);
+	}
+	return (binary_search(array, 0, size - 1, NULL));
+}
--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -20,5 +20,6 @@ struct binary_tree_s
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s avl_t;
 avl_t *sorted_array_to_avl(int *array, size_t size);
+void binary_tree_print(const binary_tree_t *);
 
 #endif /* _BINARY_TREES_H_ */
