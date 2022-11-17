576fdb113d70e8bc741b3a7da587e5c24db55c31
maycolroa <lehallo@hotmail.com>
2022-11-17 16:28:23 -0500

    0x0E. Linear search in skip list 17/11/22 0.01

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -16,11 +16,11 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 	while (_list->express)
 	{
 		printf("Value checked at index [%lu] = [%d]\n",
-			   _list->express->index, _list->express->n);
+			  _list->express->index, _list->express->n);
 		if (_list->express->n >= value)
 		{
 			printf("Value found between indexes [%lu] and [%lu]\n",
-				   _list->index, _list->express->index);
+				  _list->index, _list->express->index);
 			break;
 		}
 		_list = _list->express;
@@ -31,13 +31,13 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 		while (list->next)
 			list = list->next;
 		printf("Value found between indexes [%lu] and [%lu]\n",
-			   _list->index, list->index);
+			  _list->index, list->index);
 	}
 	list = _list;
 	while (list != _list->express)
 	{
 		printf("Value checked at index [%lu] = [%d]\n",
-			   list->index, list->n);
+			  list->index, list->n);
 		if (list->n == value)
 			break;
 		list = list->next;
