77c6d1cec89356cb763da21079ec3aa16db84357
maycolroa <lehallo@hotmail.com>
2022-11-24 11:12:33 -0500

    0x0F-starwars_api 24/11/22 0.00

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = 'https://swapi.dev/api/films/';
+
+request(url + movieId, async function (error, response, body) {
+  if (error) {
+    throw error;
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const item of characters) {
+      const response1 = await new Promise((resolve, reject) => {
+        request(item, (error, response1, body1) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(body1).name);
+          }
+        });
+      });
+      console.log(response1);
+    }
+  }
+})
---
+++ 0x0F-starwars_api/README.md
@@ -0,0 +1,1 @@
+# 0x0F. API de Star Wars
