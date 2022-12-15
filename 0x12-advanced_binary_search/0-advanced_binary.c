0daa6484f111c935e4c0256e8a970d656d0f7a0e
maycol david roa triviño <85509333+maycolroa@users.noreply.github.com>
2022-12-15 16:29:14 -0500

    Update 0-advanced_binary.c

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -7,3 +7,61 @@
  * @size: position array
  * Return: None
  */
+void print_array(int *array, size_t min, size_t size)
+{
+	size_t i;
+
+	if (min != size)
+	{
+		printf("Searching in array: ");
+		for (i = min; i < size; i++)
+		{
+			printf("%d", array[i]);
+			if (i != size - 1)
+				printf(", ");
+			else
+				printf("\n");
+		}
+	}
+}
+
+/**
+ * recursive_binary - binary search using recursion
+ * @array: array pointer
+ * @min: minimum value
+ * @max: maximum value
+ * @value: number to be searched in array
+ * Return: array index with found value, -1 if not found
+ */
+int recursive_binary(int *array, size_t min, size_t max, int value)
+{
+	int mid;
+
+	if (min > max)
+		return (-1);
+
+	print_array(array, min, max + 1);
+
+	mid = min + (max - min) / 2;
+	if (array[mid] == value && array[mid - 1] != value)
+		return (mid);
+	if (array[mid] == value && array[mid - 1] == value)
+		return (recursive_binary(array, min, mid, value));
+	if (array[mid] > value)
+		return (recursive_binary(array, min, mid, value));
+	return (recursive_binary(array, mid + 1, max, value));
+}
+
+/**
+ * advanced_binary - makes binary search with repeated numbers
+ * @array: array pointer
+ * @size: array position
+ * @value: number array
+ * Return: array index with found value, -1 if not found
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (!array || size < 1 || value < 0)
+		return (-1);
+	return (recursive_binary(array, 0, size - 1, value));
+}
