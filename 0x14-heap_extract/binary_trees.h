c8bcf3a96e732c2cafcb43f559f68c5d76a0fc57
maycolroa <lehallo@hotmail.com>
2023-01-04 21:38:06 -0500

    0x14. Heap Extract 04/01/23 0.00

---
+++ 0x14-heap_extract/binary_trees.h
@@ -0,0 +1,29 @@
+#ifndef _BINARY_TREE_
+#define _BINARY_TREE_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s heap_t;
+
+/* Function prototypes */
+int heap_extract(heap_t **root);
+
+#endif /* _BINARY_TREE_ */
