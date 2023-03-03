8351ef423f0f8a185e7750953a08b25e8aab14e8
maycolroa <lehallo@hotmail.com>
2023-03-02 23:11:59 -0500

    0x1A. Double Circular Linked List 01/03/23 0.01

--- 0x1A-double_circular_linked_list/list.h
+++ 0x1A-double_circular_linked_list/list.h
@@ -1,3 +1,11 @@
+#ifndef LIST_H
+#define LIST_H
+
+/* standard libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct List - doubly linked list
  * @str: string - (malloc'ed string)
@@ -9,7 +17,13 @@
  */
 typedef struct List
 {
-  char *str;
-  struct List *prev;
-  struct List *next;
+	char *str;
+	struct List *prev;
+	struct List *next;
 } List;
+
+/* prototype functions */
+List *add_node_end(List **list, char *str);
+List *add_node_begin(List **list, char *str);
+
+#endif /* LIST_H */
